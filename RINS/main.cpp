0ca50ca6325d1524c9b63856fb4a9ded37cb2830
slaviber <slavimber@gmail.com>
2014-02-28 03:15:33 +0200

    safe

--- RINS/main.cpp
+++ RINS/main.cpp
@@ -1,16 +1,52 @@
 #include "Graphics.h"
 #include <iostream>
 using namespace std;
-int main(int argc, char** argv) {
-	Graphics gui(640,640, "RINS");
-	int doge = gui.loadTexture("doge.jpeg");
-	cout << doge;
+
+class SDLerror{
+	const char* err;
+public:
+	SDLerror(const char* err): err(err){}
+	const char* getError(){ return err; }
+};
+
+class Game{
 	SDL_Event event;
-	bool quit = false;
-	while(!quit) {
-		while (SDL_PollEvent(&event) ) if ( event.type == SDL_QUIT ) quit = true;
+public:
+	virtual void mainLoop() = 0;
+	Game(){
+		if (SDL_Init(SDL_INIT_EVERYTHING) != 0)throw SDLerror(SDL_GetError());
+	}
+	void loop(){
+		bool quit = false;
+		while (!quit) {
+			while (SDL_PollEvent(&event)) if (event.type == SDL_QUIT) quit = true;
+			mainLoop();
+		}
+	}
+	~Game(){
+		SDL_Quit();
+	}
+};
+
+
+class RINS : public Game{
+	int doge;
+	Graphics gui;
+	void mainLoop() final{
 		gui.applyTexture(doge, 10, 100);
 		gui.renderScene();
 	}
+public:
+	RINS() : gui(640, 640, "RINS"){
+		doge = gui.loadTexture("doge.jpeg");
+		loop();
+	}
+};
+
+
+
+
+int main(int argc, char** argv) {
+	RINS rins;
 	return 0;
 }
